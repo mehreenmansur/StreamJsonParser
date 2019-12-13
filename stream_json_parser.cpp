a150b937ef5b8c0342cd3a4d48c195728956fdc4
Mehreen Mansur <mehreen.mansur@gmail.com>
2019-12-13 14:48:18 +0600

    Added timer

--- stream_json_parser.cpp
+++ stream_json_parser.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <time.h>
 #include <jsoncons/json.hpp>
 #include <jsoncons_ext/jsonpath/json_query.hpp>
 
@@ -8,6 +9,8 @@ using namespace jsoncons;
 
 int main()
 {
+    clock_t start_time = clock();
+
     std::ifstream is("data.json");
     json data = json::parse(is);
 
@@ -16,7 +19,15 @@ int main()
     cin >> query;
 
     json result = jsonpath::json_query(data, "$." + query);
-    cout << result[0].as<std::string>() << endl;
+
+    clock_t end_time = clock();
+    int time_diff = end_time - start_time;
+
+    cout << "-----------------------------------------------" <<endl;
+    cout << "Parsed in " << time_diff/1000.0 << " second(s)." <<endl;
+    cout << "-----------------------------------------------" <<endl;
+
+    cout << query << " : " << result[0].as<std::string>() << endl<<endl;
 
     return 0;
 }
